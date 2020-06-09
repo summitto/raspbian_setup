a355c0e727f36744b186641527b75b5ab5801552
vicsn <vicsn@users.noreply.github.com>
2020-06-09 18:15:06 +0200

    add build directories to path

--- start.sh
+++ start.sh
@@ -21,3 +21,5 @@ cd ~/pgp-packet-library/build && cmake .. && make && sudo make install
 mkdir -p ~/pgp-key-generation/build
 cd ~/pgp-key-generation/build && cmake .. && make
 
+# add build directories to path
+export "~/pgp-key-generation/build/generate_derived_key:~/pgp-key-generation/build/extend_key_expiry:$PATH"
